2e6d971550045c8956b7bee8450fb89d30871633
marco-bertelli <marcobert37@gmail.com>
2020-04-17 11:31:07 +0200

    inizio lettura da db

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaComponent } from './lista/lista/lista.component';
 
 //questi sono i routes base man mano che create le cose e vi servono scommentate queste qua sotto e modificatele a piacimento
 const routes: Routes = [
@@ -7,7 +8,7 @@ const routes: Routes = [
    // { path: 'home', component: HomeComponent },
   //  { path: 'funzionalita', component: FunzionalitaComponent, canActivate: [MyRouteGuardService]},
   //  { path: 'crealista', component: CrealistaComponent, canActivate: [MyRouteGuardService]},
-  //  { path: 'lista', component: ListaComponent, canActivate: [MyRouteGuardService]},
+      { path: 'lista', component: ListaComponent},
   //  { path: 'dettaglio/:id', component: DettaglioComponent, canActivate: [MyRouteGuardService]},
   //  { path: 'registrati', component:RegistrazioneComponent, canActivate: [EditRouteGuard]},
   //  { path: '', redirectTo: '/home', pathMatch: 'full' },
---
+++ src/app/models/ListItem.ts
@@ -0,0 +1,12 @@
+export interface ListItem{
+    Id :number;
+    Nome:string;
+    autore:string;
+    Genere:number;
+    Rating:number;
+    riproduzioni:number;
+    Annouscita:Date;
+    nascosto:boolean;
+    preferito:boolean;
+
+}
