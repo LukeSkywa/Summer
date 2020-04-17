cde4eb4cafeadadf6002011d4309d73473efe31c
marco-bertelli <marcobert37@gmail.com>
2020-04-17 10:25:28 +0200

    creazione routes di base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+//questi sono i routes base man mano che create le cose e vi servono scommentate queste qua sotto e modificatele a piacimento
+const routes: Routes = [
+   // { path: 'login', component: LoginComponent,canActivate: [EditRouteGuard] },
+   // { path: 'home', component: HomeComponent },
+  //  { path: 'funzionalita', component: FunzionalitaComponent, canActivate: [MyRouteGuardService]},
+  //  { path: 'crealista', component: CrealistaComponent, canActivate: [MyRouteGuardService]},
+  //  { path: 'lista', component: ListaComponent, canActivate: [MyRouteGuardService]},
+  //  { path: 'dettaglio/:id', component: DettaglioComponent, canActivate: [MyRouteGuardService]},
+  //  { path: 'registrati', component:RegistrazioneComponent, canActivate: [EditRouteGuard]},
+  //  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  //  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+ }
