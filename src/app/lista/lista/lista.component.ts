c415e4d57148aa9a571c51488782a9bb9a0d6310
marco-bertelli <marcobert37@gmail.com>
2020-04-17 15:16:14 +0200

    filtra finito con lettura da server

--- src/app/lista/lista/lista.component.ts
+++ src/app/lista/lista/lista.component.ts
@@ -11,7 +11,7 @@ import { ListItem } from 'src/app/models/ListItem';
 export class ListaComponent implements OnInit {
   lista=[];
   constructor(private myHttpService: MyHttpService) { }
-
+//riempi la lista da db
   riempi(){
     this.myHttpService.getCanzoni().subscribe(reponse => {
       this.lista = reponse;
@@ -19,6 +19,19 @@ export class ListaComponent implements OnInit {
     }, err => {
       console.log('error');
     });
+  }
+  //riempi lista filtrata
+  retrieveFilter(value){
+    this.myHttpService.getFiltra(value).subscribe(reponse => {
+      this.lista = reponse.body;
+    }, err => {
+      console.log('error');
+    });
+  }
+  filtra(value:string){
+    if(value==="all") this.riempi();
+    else this.retrieveFilter(value);
+
   }
   ngOnInit(): void {
     this.riempi();
--- src/app/services/myhttp.service.ts
+++ src/app/services/myhttp.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ListItem } from '../models/ListItem';
 
@@ -9,6 +9,18 @@ import { ListItem } from '../models/ListItem';
   providedIn: 'root'
 })
 export class MyHttpService {
+   //funzione che filtra
+   getFiltra(value: string): Observable<HttpResponse<ListItem[]>> {
+    //stringa di comodo
+    
+    let params: HttpParams;
+
+    if(value==="pref") params = new HttpParams().set('preferito', "true");
+
+    else params = new HttpParams().set('nascosto', "true");
+
+    return this.httpClient.get<ListItem[]>('http://localhost:3000/canzoni', { observe: 'response',  params: params });
+  }
 
   constructor(private httpClient: HttpClient) { }
 
