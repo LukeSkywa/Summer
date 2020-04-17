fa889dd35243e4a1b3ca34ecffc15e0a7ef45455
xrago-afk <lorza2000@gmail.com>
2020-04-17 14:20:58 +0200

    punto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { ListaComponent } from './lista/lista/lista.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
-=======
 import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
->>>>>>> cb70bb2d73589172ea24d2622c036ca6c4a03d80
 
 @NgModule({
   declarations: [
@@ -23,12 +20,9 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    HttpClientModule
-=======
+    HttpClientModule,
     FormsModule,
     ReactiveFormsModule
->>>>>>> cb70bb2d73589172ea24d2622c036ca6c4a03d80
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import { MenuItem } from '../../models/menu-item.interface';
+
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @Output()
+  selectMenuItem:EventEmitter<number>=new EventEmitter<number>();
+
+  menulist:MenuItem[] =[
+    {id:1,description:"Home",selected:true },
+    {id:2,description:"Lista",selected:false },
+    {id:3,description:"Cards",selected:false },
+    {id:4,description:"Feedback",selected:false },
+    {id:5,description:"Profilo",selected:false },
+    {id:2,description:"Esci",selected:false },
+  ]
+  selectComponet(id:number){
+    this.selectMenuItem.emit(id);
   }
 
 }
