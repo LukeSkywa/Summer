eb531e89be5780070e290b43d61d0805071e400a
xrago-afk <lorza2000@gmail.com>
2020-04-20 17:03:02 +0200

    Rooting funzionante menu base

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,8 +10,8 @@ import { MenuItem } from 'src/app/models/menu-item.interface';
 export class MenuComponent implements OnInit {
 
   menuList: MenuItem[] = [
-    {id: 1, description: 'Home', url:"/Home"},
-    {id: 2, description: 'Lista', url:"/List"},
+    {id: 1, description: 'Home', url:"/home"},
+    {id: 2, description: 'Lista', url:"/lista"},
     {id: 3, description: 'Cards', url:"/Cards"},
   ];
    
