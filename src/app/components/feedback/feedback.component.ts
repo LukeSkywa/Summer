94e5a3c43efcaeab792247e2bbd40e92da2f490c
marco-bertelli <marcobert37@gmail.com>
2020-04-22 13:07:22 +0200

    invio con email funzionante

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import { HttpHeaders } from '@angular/common/http';
+import { MyHttpService } from 'src/app/services/myhttp.service';
 
 @Component({
   selector: 'app-feedback',
@@ -10,16 +12,31 @@ export class FeedbackComponent implements OnInit {
 
   feedform:FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,private myHttpService: MyHttpService) {
     this.feedform = this.fb.group({
       nome: '',
       cognome: '',
       genere:'',
       email:'',
-      telelfono:'',
+      telefono:'',
       commenti:'',
     });
    }
+
+   onSubmit(contactForm) {
+    if (contactForm.valid) {
+      const email = contactForm.value;
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+      this.myHttpService.invia('https://formspree.io/mwkngpoo',
+        { name: email.name, replyto: email.email, message: email.messages },
+        { 'headers': headers }).subscribe(
+          response => {
+            window.alert("invio effettuato");
+            console.log(response);
+          }
+        );
+    }
+  }
 
   ngOnInit(): void {
   }
